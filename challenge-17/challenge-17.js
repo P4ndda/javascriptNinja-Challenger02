41f5c2217d52447fcba0132b2dad1a701d09c002
Pedro Augusto <96143899+P4ndda@users.noreply.github.com>
2022-12-06 20:21:27 -0300

    Responding

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -20,6 +20,10 @@ no console:
 console.log( 'Adicionando seu nome no texto:' );
 // ?
 
+var text = "Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de 'O Centauro de Luvas', foi um militar, político, abolicionista e monarquista brasileiro.";
+text = text.replace('Manuel Marques de Sousa', 'Pedro Augusto Adão Silva')
+console.log('Adicionando seu nome no texto:', text);    
+
 /*
 Agora, substitua a palavra "brasileiro" por sua cidade natal e mostre no
 console.
@@ -28,12 +32,18 @@ Ex: Se você for da São Paulo, substitua por "paulista".
 console.log( '\nTrocando naturalidade:' );
 // ?
 
+text = text.replace(/brasileiro/g, 'Uberlandense');
+console.log('\nTrocando naturalidade:' ,text);
+
 /*
 Substitua todos os números por um traço `-`. Cada caractere de número deve
 ser um traço. Mostre o resultado no console:
 */
 console.log( '\nTrocando números por -:' );
 // ?
+
+text = text.replace(/\d/g, '-');
+console.log('Trocando números por traços: ', text);
 
 /*
 Substitua todas as letras (somente letras) de "D" maiúsculo até "h"
@@ -42,6 +52,9 @@ minúsculo por "0" (número zero). Mostre o resultado no console:
 console.log( '\nTrocando de "D" a "h" por "0":' );
 // ?
 
+  text = text.replace(/[D a h]/g, '0');
+  console.log('Trocando de "D" a "h" por "0": ', text);
+
 /*
 Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
 Mostre o resultado no console:
@@ -49,12 +62,20 @@ Mostre o resultado no console:
 console.log( '\nTrocando "A" e "a" por "4":' );
 // ?
 
+  console.log('Trocando de "A" e "a" por "": ', text.replace(/[Aa]/g, '4'));
+
+
 /*
 Substitua a frase "O Centauro de Luvas", deixando-a em caixa alta, usando
 o método `toUpperCase()`. Mostre o resultado no console:
 */
 console.log( '\n"O Centauro de Luvas" em caixa alta:' );
 // ?
+
+  console.log('"O Centauro de Luvas" em caixa alta: ',
+    text = text.replace(/O Centauro de Luvas/g, function(frase){
+      return frase.toUpperCase();
+  }));
 
 /*
 Agora iremos substituir as datas no formato "13 de junho de 1804" para
@@ -70,6 +91,61 @@ Use um console.log para cada mês, usando a frase:
 */
 console.log( '\nMeses representados por números:' );
 // ?
+
+  function getMonthNumber(nameMonth){
+      var numberMonth;
+      switch(nameMonth){
+          case 'Janeiro':
+               numberMonth = '01'; 
+          break;
+          case 'Fevereiro':
+               numberMonth = '02';
+          break;
+          case 'Marco':
+               numberMonth = '03';
+          break;
+          case 'Abril':
+               numberMonth = '04';
+          break;
+          case 'Maio':
+               numberMonth = '05';    
+          break;
+          case 'Junho':
+               numberMonth = '06';
+          break;
+          case 'Julho':
+               numberMonth = '07';
+          break;
+          case 'Agosto':
+               numberMonth = '08';
+          break;
+          case 'Setembro':
+               numberMonth = '09';
+          break;
+          case 'Outubro':
+              numberMonth = '10';    
+          break;
+          case 'Novembro':
+              numberMonth = '11';    
+          break;
+          case 'Dezembro':
+              numberMonth = '12';    
+          break;           
+      }
+
+    return numberMonth;
+  };console.log('O mês de [Janeiro] é representado pelo número ', getMonthNumber('Janeiro'));
+console.log('O mês de [Fevereiro] é representado pelo número ', getMonthNumber('Fevereiro'));
+console.log('O mês de [Marco] é representado pelo número ', getMonthNumber('Marco'));
+console.log('O mês de [Abril] é representado pelo número ', getMonthNumber('Abril'));
+console.log('O mês de [Maio] é representado pelo número ', getMonthNumber('Maio'));
+console.log('O mês de [Junho] é representado pelo número ', getMonthNumber('Junho'));
+console.log('O mês de [Julho] é representado pelo número ', getMonthNumber('Julho'));
+console.log('O mês de [Agosto] é representado pelo número ', getMonthNumber('Agosto'));
+console.log('O mês de [Setembro] é representado pelo número ', getMonthNumber('Setembro'));
+console.log('O mês de [Outubro] é representado pelo número ', getMonthNumber('Outubro'));
+console.log('O mês de [Novembro] é representado pelo número ', getMonthNumber('Novembro'));
+console.log('O mês de [Dezembro] é representado pelo número ', getMonthNumber('Dezembro'));
 
 /*
 Agora, declare uma variável chamada `regexDate` que irá receber a expressão
