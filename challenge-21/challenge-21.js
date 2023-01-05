36ff0e82715a535ce5f980dcc57838eb6d71df51
Pedro Augusto <96143899+P4ndda@users.noreply.github.com>
2023-01-05 17:56:48 -0300

    Wait, i need to make improvements

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -15,3 +15,68 @@ usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
 // ?
+
+<body>
+    <div class="stopWatch">
+        <h2 data-js="visor">00:00:00</h2>
+        <div class="buttons">
+            <button data-js="start">start</button>
+            <button data-js="stop">stop</button>
+            <button data-js="reset">reset</button>
+        </div>
+    </div>
+    <script>
+        (function(doc, win){
+            'use strict';
+
+            var visor = doc.querySelector('[data-js="visor"]');
+            var start = doc.querySelector('[data-js="start"]');
+            var stop = doc.querySelector('[data-js="stop"]');
+            var reset = doc.querySelector('[data-js="reset"]');
+            var hour = 0, minute = 0 ,secund = 0;
+            var count = 0; 
+            var timer;
+
+            function twoDigits(number){
+                return number > 10? number : `0${number}`;
+            }
+
+            function starting(){ 
+                count = 1;
+                secund++;
+
+                secund > 10
+                minute > 10
+                hour > 10
+
+                if(secund > 60)
+                    minute++, secund = 0;
+                if(minute > 60)
+                    hour++, minute = 0;
+                if(hour > 24)
+                    hour = 0;
+
+                timer = setTimeout(starting, 10);
+                visor.innerHTML = `${twoDigits(hour)}:${twoDigits(minute)}:${twoDigits(secund)}`
+            }
+
+            if(count == 0){
+                start.addEventListener('click', starting, false);
+            }
+
+                stop.addEventListener('click', function(){
+                    clearTimeout(timer);
+                    count = 0;
+                }, false)
+
+                reset.addEventListener('click', function(){
+                    secund = 0;
+                    minute = 0; 
+                    hour = 0;
+                    
+                    visor.innerHTML = `0${hour}:0${minute}:0${secund}`                    
+                }, false);
+            
+        })(document, window);
+    </script>
+</body>
