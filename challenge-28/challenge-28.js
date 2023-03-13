8e2a000d908bfee89400aa3e6cb19b44f327c317
Pedro Augusto <96143899+P4ndda@users.noreply.github.com>
2023-03-13 14:41:12 -0300

    new functions

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -25,50 +25,98 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
-        (function(doc, win){            
+<body>
+    <form action="" method="get">
+        <label for="iCep">Cep: </label>
+        <input type="text" name="" id="iCep" minlength="8">
+
+        <input type="submit" value="Enviar" id="iSubm">
+
+        <div>
+            <h1 data-js="answer"></h1>
+        </div>
+
+        <label for="iLog">Rua: </label>
+        <input type="text" name="" id="iLog" data-js = 'addres'>
+
+        <label for="iBai">Bairro: </label>
+        <input type="text" name="" id="iBai" data-js = 'addres'>
+
+        <label for="iEst">Estado: </label>
+        <input type="text" name="" id="iEst" data-js = 'addres'>
+
+        <label for="iCid">Cidade: </label>
+        <input type="text" name="" id="iCid" data-js = 'addres'>
+    </form>
+    <script>
+
+        (function (doc, win) {
             var $cep = doc.querySelector('#iCep');
             var $subm = doc.querySelector('#iSubm');
             var answe = doc.querySelector('[data-js="answer"]');
-            
-            function getRegexCep(){
-                $cep = $cep.value.match(/\d/g).join('');
-                return $cep;
+
+            function getRegexCep() {
+                $cep = doc.querySelector('#iCep');
+                $cep = $cep.value;
+                $cep = $cep.match(/\d{2,5}/g).join('');
+                return $cep
             }
 
-            function completeAddr(request){
+            function completeAddr(request){                    answe.innerHTML = answerStat('ok');
                 var $Log = doc.querySelector('#iLog'); $Log.value = JSON.parse(request.responseText).logradouro;
-                var $Bai = doc.querySelector('#iBai'); $Bai.value = JSON.parse(request.responseText).bairro;  
+                var $Bai = doc.querySelector('#iBai'); $Bai.value = JSON.parse(request.responseText).bairro;
                 var $Est = doc.querySelector('#iEst'); $Est.value = JSON.parse(request.responseText).uf;
                 var $Cid = doc.querySelector('#iCid'); $Cid.value = JSON.parse(request.responseText).localidade;
-            }
-            
-            function getRequest(){
-                var viaCep = new XMLHttpRequest();
-                viaCep.open('GET', `https://viacep.com.br/ws/${getRegexCep()}/json/`);
-                viaCep.send('');
-                viaCep.addEventListener('readystatechange', function(){requestIsOk(viaCep);});
+                return true;
             }
 
-            function answerStat(type){
-                return {
-                    ok:  `encontramos o endereço para o CEP: ${$cep}`,
-                    error: `Não encontramos o endereço para o CEP: ${$cep}`,
-                    searching: `Buscando informações para o CEP ${$cep}`
-                }[type];
-            }
-
-            $subm.addEventListener('click', function(e){
-                e.preventDefault();
-                getRequest()
-            });
-
-            function requestIsOk(request){
-                if((request.readyState == 4 && request.status == 200 == true)){
-                    completeAddr(request)
-                    answe.innerHTML = answerStat('ok');
-                }else{
-                    answe.innerHTML = answerStat('error');
+            function clearAddr(){
+                var address = doc.querySelectorAll('[data-js = "addres"]');
+                if(address[0].value){
+                    address.forEach(function(item){
+                        item.value = '';
+                    })
                 }
             }
 
+            function thisCepExist(request) {
+                if(JSON.parse(request.responseText).erro == true){
+                    answe.innerHTML = answerStat('error');
+                    clearAddr();
+                }else
+                    completeAddr(request)
+            }
+
+            function getRequest(event) {
+                var viaCep = new XMLHttpRequest();
+                viaCep.open('GET', `https://viacep.com.br/ws/${getRegexCep()}/json`, true);
+                viaCep.send();
+                viaCep.addEventListener('readystatechange', function () { requestIsOk(viaCep) })
+            }
+
+            function answerStat(type) {
+                return {
+                    ok: `encontramos o endereço para o CEP: ${$cep}`,
+                    error: `Não encontramos o endereço para o CEP: ${$cep}`,
+                    doesntExis: `CEP: ${$cep}, Invalido por favor tente novamente`,
+                    searching: `Buscando informações para o CEP`
+                }[type];
+            }
+
+            $subm.addEventListener('click', function (e) {
+                answe.innerHTML = answerStat('searching')
+                e.preventDefault();
+                getRequest();
+            });
+
+            function requestIsOk(request) {
+                if((request.readyState == 4 && request.status == 200) == true)
+                    thisCepExist(request)
+                else
+                    answe.innerHTML = answerStat('doesntExis');
+            }
+
         })(document, window);
+
+    </script>
+</body>
